d79f697d81b3b40c19ef3d807fca4c2ac5de8d57
Siven Sadiyan <siven76@hotmail.com>
2019-07-07 18:48:14 +0400

    Add /country/:country/zone route and function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,7 @@ admin.initializeApp(functions.config().firebase);
 
 const app = express();
 const main = express();
+const db = admin.firestore();
 
 main.use('/api/v1', app);
 main.use(bodyParser.json());
@@ -17,4 +18,34 @@ app.get('/test', (request, response) => {
 
     response.send('Mauritius Post - PAC API');
 
-})
+});
+
+app.get('/country/:country/zone', async (request, response) => {
+    try {
+        const country = request.params.country;
+
+        if (!country) throw new Error('Country name is required');
+
+        const zoneCountrySnapshot = await db.collection("zone_contries")
+            .where("country", "==", country)
+            .get();
+
+        const results: any[] | { id: string; data: FirebaseFirestore.DocumentData; }[] = [];
+
+        zoneCountrySnapshot.forEach(
+            (doc) => {
+                results.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            }
+        );
+
+        response.json(results);
+
+    } catch (error) {
+
+        response.status(500).send(error);
+
+    }
+});
